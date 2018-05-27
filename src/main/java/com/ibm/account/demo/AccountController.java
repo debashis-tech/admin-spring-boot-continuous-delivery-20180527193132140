1fe6b761bc33e77ba07e1af6099850d37326984d
debashis-tech <debashis.tech@gmail.com>
2018-05-28 01:06:55 +0530

    4

--- src/main/java/com/ibm/account/demo/AccountController.java
+++ src/main/java/com/ibm/account/demo/AccountController.java
@@ -7,9 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.cloudant.client.api.Database;
-import com.cloudant.client.api.model.Response;
-
 import java.util.List;
 import java.util.ArrayList;
 import java.io.*;
@@ -20,22 +17,8 @@ public class AccountController {
 	@Autowired
 	Database account;
 	
-	@RequestMapping(path="/", method=RequestMethod.GET)
-	public String allAccounts(Model listModel) {
-		List<Account> allAccounts;
-		List<String> accountNames = new ArrayList<String>();
-		try{
-			allAccounts = account.getAllDocsRequestBuilder().includeDocs(true).build()
-                		.getResponse().getDocsAs(Account.class);
-			listModel.addAttribute("allAccounts", allAccounts);
-			for (int i=0; i<allAccounts.size(); i++){	
-				accountNames.add(allAccounts.get(i).getName() + " - " + allAccounts.get(i).getId());
-			}
-			listModel.addAttribute("accountNames", accountNames);
-		}catch(IOException ioe){
-			System.out.println("Unable to find documents.");
-		}
-		
-		return "allAccounts";
+	@RequestMapping(path="/test", method=RequestMethod.GET)
+	public String test() {
+		return "THIS IS A TEST!!!";
 	}
 }

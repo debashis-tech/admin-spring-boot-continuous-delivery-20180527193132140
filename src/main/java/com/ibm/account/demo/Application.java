58d70543f2d2bf60dbb702416907c516a57f3500
debashis-tech <debashis.tech@gmail.com>
2018-05-28 01:07:24 +0530

    5

--- src/main/java/com/ibm/account/demo/Application.java
+++ src/main/java/com/ibm/account/demo/Application.java
@@ -5,19 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
-import com.cloudant.client.api.CloudantClient;
-import com.cloudant.client.api.Database;
-
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
-	}
-	
-
-	@Bean
-	public Database account(CloudantClient cloudant) {
-		return cloudant.database("account", true);
 	}
 }
